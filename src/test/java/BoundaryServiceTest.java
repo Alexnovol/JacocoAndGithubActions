8bd3d9889966f67b1067fe978d98d35f8de18a1b
Alexnovol <alexxnovol@icloud.com>
2024-05-28 14:57:14 +0300

    Внес правки в соответствии с замечаниями

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -1,16 +1,38 @@
 import org.example.BoundaryService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BoundaryServiceTest {
 
     private BoundaryService boundaryService = new BoundaryService();
 
-    @Test
-    @DisplayName("Метод findMin возвращает минимальное число массива")
-    public void shouldReturnMinNum() {
-        int[] nums = {5, 2, 6, 7, 8, 9, 10, 100, 25};
-        assertEquals(2, boundaryService.findMin(nums));
+
+    @ParameterizedTest(name = "{index}: Число {1} минимальное в {0}")
+    @MethodSource("argsProviderFactoryForPositiveChecks")
+    public void shouldReturnMinNum(int[] numbers, int minNum) {
+        assertEquals(minNum, boundaryService.findMin(numbers));
     }
+
+    @Test
+    @DisplayName("Возникает исключение, если массив пуст")
+    public void shouldThrowExceptionWithEmptyArray() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> boundaryService.findMin(new int[0]));
+    }
+
+    static Stream<Arguments> argsProviderFactoryForPositiveChecks() {
+        int[] numbers1 = {20, 1, 50, 60, 5};
+        int[] numbers2 = new int[3];
+        return Stream.of(Arguments.of(numbers1, Arrays.stream(numbers1).min().getAsInt()),
+                Arguments.of(numbers2, Arrays.stream(numbers2).min().getAsInt()));
+    }
+
+
 }
